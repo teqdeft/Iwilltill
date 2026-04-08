1ee4a6a5f1a401438726b0a7a6e69a2135224f6a
teqdeft <teqdeft@gmail.com>
2026-04-08 12:54:57 +0530

    Favicon update

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,11 @@ import "./globals.css";
 
 export const metadata = {
   title: "iWILL 'til i'mWELL | Medical & Mental Healthcare",
+  icons: {
+    icon: "images/logo-favi.svg",
+    shortcut: "images/logo-favi.svg",
+    apple: "images/logo-favi.svg",
+  },
   description:
     "Access doctors, therapists, and board-certified providers on your terms. Self-care is the new healthcare. Simple, affordable, and on your time.",
   keywords: [
